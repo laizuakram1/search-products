21b2abdbacb3bd46a9f61c9f5ee96ed3f47488c3
laizuakram1 <laizuakram98@gmail.com>
2022-03-18 17:53:36 +0600

    add boxshadow in products card

--- src/components/products/product/Product.js
+++ src/components/products/product/Product.js
@@ -7,7 +7,7 @@ const Product = (props) => {
              <img width='260px' height="70%" src={props.image} alt="" />
             <h2 className='mt-3'>{props.title}</h2>
             <h4 className='text-warning'>$$ {props.price}</h4>
-            <button className='btn btn-primary me-3'>Add cart</button>
+            <button onClick={AddCart} className='btn btn-primary me-3'>Add cart</button>
             <button className='button btn btn-danger me-3'>Delete</button>
             <button className='button btn btn-info me-3'>Details</button>
            
