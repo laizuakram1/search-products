6744c07f293606e4b62dc2e4a0538280ab01b897
laizuakram1 <laizuakram98@gmail.com>
2022-03-18 23:01:16 +0600

    add react bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Menubar from './components/menubar/Menubar';
 import Products from './components/products/Products';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Menubar></Menubar>
       <Products></Products>
+      
      
     </div>
   );
--- src/components/products/product/Product.js
+++ src/components/products/product/Product.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Product.css'
+import AddCart from '../../AddToCart/AddCart';
 const Product = (props) => {
     
     return (
         <div className='product-container'>
-             <img width='260px' height="70%" src={props.image} alt="" />
+             <img width='250px' height="60%" src={props.image} alt="" />
             <h2 className='mt-3'>{props.title}</h2>
             <h4 className='text-warning'>$$ {props.price}</h4>
-            <button onClick={AddCart} className='btn btn-primary me-3'>Add cart</button>
+            <button onClick={<AddCart></AddCart>} className='btn btn-primary me-3'>Add cart</button>
             <button className='button btn btn-danger me-3'>Delete</button>
             <button className='button btn btn-info me-3'>Details</button>
            
