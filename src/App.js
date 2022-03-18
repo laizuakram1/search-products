f26cf518bb0db9ad1f7e1aa8705ef4e3299f97ff
laizuakram1 <laizuakram98@gmail.com>
2022-03-18 12:09:46 +0600

    menubar items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Menubar from './components/menubar/Menubar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello from  Laizu</h2>
+      <Menubar></Menubar>
     </div>
   );
 }
---
+++ src/components/menubar/Menubar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Menubar = () => {
+    return (
+        <div>
+            <nav>
+                <div>Logo</div>
+                <div>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                    <li>About</li>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Menubar;
